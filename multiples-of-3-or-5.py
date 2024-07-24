78cb8bc59a192c6bfc6bc557e3db2c03b26e6d94
peedrovzxf <166252531+peedrovzxf@users.noreply.github.com>
2024-07-24 00:29:24 -0400

    feat: add multiples of 3 or 5 problem solve

---
+++ multiples-of-3-or-5.py
@@ -0,0 +1,13 @@
+"""
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+sum = 0
+
+for i in range(1000):
+    if i % 3 != 0 and i % 5 != 0:
+        continue
+
+    sum += i
+
+print(sum)
